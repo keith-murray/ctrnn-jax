5506cbaecc08ccba6b303f066e48af23520e52ef
Keith T. Murray <murraykeith64@gmail.com>
2024-10-16 22:31:28 +0200

    Added dynamics showcase

--- ctrnn_jax/model.py
+++ ctrnn_jax/model.py
@@ -10,7 +10,7 @@ from jax import random
 from flax.linen import initializers
 from flax.linen.linear import Dense
 from flax.linen.recurrent import RNNCellBase
-from flax.linen.activation import tanh
+from flax.linen.activation import tanh, sigmoid
 from flax.linen.module import compact, nowrap
 from flax.typing import PRNGKey, Dtype, Initializer
 
@@ -128,6 +128,117 @@ class CTRNNCell(RNNCellBase):
             mem_shape,
             self.param_dtype,
         )
+
+    @property
+    def num_feature_axes(
+        self,
+    ) -> int:
+        return 1
+
+
+# pylint: disable=too-many-function-args
+class SimpleCTRNNCell(RNNCellBase):
+    """
+    A simplified continuous-time recurrent neural network (CTRNN) cell
+    discretized and numerically integrated with Euler's method.
+
+    This version removes the noise injection and output layer found
+    in more complex CTRNN implementations and uses an array of taus
+    (time constants) for each neuron.
+
+    Attributes:
+        hidden_features (int): Number of hidden neurons.
+        dt (jnp.float32): Discretization factor (time step) for Euler integration.
+    """
+
+    hidden_features: int
+    dt: jnp.float32
+    activation_fn: Callable[..., Any] = sigmoid
+    kernel_init: Initializer = initializers.glorot_normal()
+    bias_init: Initializer = initializers.zeros_init()
+    dtype: Dtype | None = None
+    param_dtype: Dtype = jnp.float32
+    carry_init: Initializer = initializers.zeros_init()
+    tau_init: Initializer = initializers.ones_init()
+
+    @compact
+    def __call__(
+        self,
+        carry,
+        task_input,
+    ):
+        """
+        Perform a forward pass for the CTRNN cell at a single time step.
+
+        This method updates the hidden state based on the previous state (carry),
+        the current input, and the array of neuron-specific time constants (taus).
+        The update is computed using Euler's method.
+
+        Args:
+            carry (tuple of jnp.ndarray): A tuple containing:
+                - hidden_activations (jnp.ndarray): Hidden state from the previous time step (t-1).
+                - tau (jnp.ndarray): An array of time constants for each neuron.
+            task_input (jnp.ndarray): Input at the current time step (t).
+
+        Returns:
+            tuple containing:
+                - hidden_activations (jnp.ndarray): The hidden state from the current time step (t).
+                - tau) (jnp.ndarray): An array of time constants for each neuron.
+            hidden_activations (jnp.ndarray): The hidden state from the current time step (t).
+        """
+        hidden_activations, tau = carry
+        dt_tau = self.dt / tau
+
+        dense_h = partial(
+            Dense,
+            features=self.hidden_features,
+            use_bias=True,
+            kernel_init=self.kernel_init,
+            bias_init=self.bias_init,
+            dtype=self.dtype,
+            param_dtype=self.param_dtype,
+        )
+
+        rates_input = self.activation_fn(
+            dense_h(name="recurrent_kernel")(hidden_activations)
+        )
+        rates_hidden = (jnp.float32(1.0) - dt_tau) * hidden_activations + dt_tau * (
+            rates_input + task_input
+        )
+
+        return (rates_hidden, tau), rates_hidden
+
+    @nowrap
+    def initialize_carry(self, rng: PRNGKey, input_shape: tuple[int, ...]):
+        """
+        Initialize the carry (hidden state and tau) for the CTRNN cell.
+
+        This method initializes both the hidden activations and the array of taus
+        for each neuron. The hidden activations are initialized using the carry_init
+        method, while the taus are initialized using tau_init.
+
+        Args:
+            rng (PRNGKey): Random number generator used for initialization.
+            input_shape (tuple[int]): a tuple providing the shape of the input to the cell.
+
+        Returns:
+            tuple containing:
+                - hidden_activations (jnp.ndarray): Initialized activations (zeros by default).
+                - tau (jnp.ndarray): Initialized tau values (ones by default).
+        """
+        batch_dims = input_shape[:-1]
+        mem_shape = batch_dims + (self.hidden_features,)
+        carry_init = self.carry_init(
+            rng,
+            mem_shape,
+            self.param_dtype,
+        )
+        tau_init = self.tau_init(
+            rng,
+            mem_shape,
+            self.param_dtype,
+        )
+        return (carry_init, tau_init)
 
     @property
     def num_feature_axes(
